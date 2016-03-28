12c05befbb464f3c856594d871db989ea6072d89
xll216 <xll_216@163.com>
2016-03-28 16:37:16 +0800

    提交到develop分支上

---
+++ app/src/main/java/com/xradiogroup/TestActivity.java
@@ -0,0 +1,15 @@
+package com.xradiogroup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lilyxiao on 16/3/28.
+ */
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
